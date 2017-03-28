138cb0fbd04842a44367bd81f60d9cf1f318b970
kkayyoo <yokoyau159@gmail.com>
2017-03-27 21:47:39 -0400

    Adding css style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,13 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  //Or using inline style
+  // styles: [`
+  //   h3 {
+  //     color: blue;
+  //   }
+  // `]
 })
 export class AppComponent {
-  
+
 }
